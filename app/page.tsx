432de2c92dd0fe0168a17bf57a2613da99a37f16
Yuvaraj-96 <yuvarajmechmailam@gmail.com>
2023-08-16 22:05:58 +0530

    added pdf reader partial code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import { Hero } from '@/components'
 import Image from 'next/image'
+import { pdfjs } from 'react-pdf';
 
 export default function Home() {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+  ).toString();
+
+  
+
   return (
     <main className='overflow-hidden'>
       <Hero/> 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,48 @@
 "use client"
 import React from 'react'
 import { CustomButton } from '.'
+import Image from 'next/image'
+import {useState,ChangeEvent} from 'react'
 
 const Hero = () => {
+  const [file, setFile] = useState<File>(); 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => { 
+    if (e.target.files) { 
+      setFile(e.target.files[0]);
+     }
+   };
+     const handleUploadClick = () => { 
+      if (!file) { 
+        return; 
+      }
+    }
   return (
-    <div className='hero'>
-      <div className="flex-1 pt-36 padding-x">
-        <h1 className='hero__title'>
-          Find, book, or rent a car --quickly and easily!
-        </h1>
-        <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
-        <CustomButton 
-          title="Explore cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={()=>{}} />
-      </div>
-    </div>
+    <div> 
+        <input type="file" onChange={handleFileChange} /> 
+        <div>{file && `${file.name} - ${file.type}`}</div>
+        <button onClick={handleUploadClick}>Upload</button>
+        </div>
   )
+  // (
+  //   <div className='hero'>
+  //     <div className="flex-1 pt-36 padding-x">
+  //       <h1 className='hero__title'>
+  //         Find, book, or rent a car --quickly and easily!
+  //       </h1>
+  //       <p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
+  //       <CustomButton 
+  //         title="Explore cars"
+  //         containerStyles="bg-primary-blue text-white rounded-full mt-10"
+  //         handleClick={()=>{}} />
+  //     </div>
+  //     <div className='hero__image-container'>
+  //       <div className='hero__image'>
+  //         <Image src='/../public/hero.png' alt="hero" fill className='object-contain' />
+  //         <div className='hero__image-overlay'></div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 }
 
 export default Hero
